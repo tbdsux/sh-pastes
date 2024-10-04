45ed3a18845216447d316af0df86334d07cbb8c1
Joshue Abance <iamcoderx@gmail.com>
2024-10-05 00:57:44 +0800

    setup dockerfile and compose

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM golang:1.23
+
+WORKDIR /app
+
+# pre-copy/cache go.mod for pre-downloading dependencies and only redownloading them in subsequent builds if they change
+COPY go.mod go.sum ./
+RUN go mod download && go mod verify
+
+COPY . .
+RUN go build -v -o app .
+
+
+EXPOSE 9999
+
+CMD ["./app"]
---
+++ api/main.go
@@ -0,0 +1,57 @@
+package api
+
+import (
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/storage/postgres/v3"
+	gonanoid "github.com/matoous/go-nanoid/v2"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+// Run starts the API server
+func Run() {
+	app := fiber.New()
+	store := postgres.New(postgres.Config{
+		Host:     "postgres",
+		Database: "shpastes",
+		Table:    "pastes",
+		Username: "postgres",
+		Password: os.Getenv("POSTGRES_PASSWORD"),
+		Port:     5432,
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString(`SH Raw Pastes Hosting
+
+
+cat file.txt | curl -X POST --data-binary @- {{host}}'
+		`)
+
+	})
+
+	app.Post("/", func(c *fiber.Ctx) error {
+		id, err := gonanoid.New()
+		if err != nil {
+			return c.SendStatus(500)
+		}
+
+		if err := store.Set(id, c.BodyRaw(), 0); err != nil {
+			return c.SendStatus(500)
+		}
+
+		return c.SendString(id)
+	})
+
+	app.Get("/:id", func(c *fiber.Ctx) error {
+		data, err := store.Get(c.Params("id"))
+		if err != nil {
+			return c.SendStatus(404)
+		}
+
+		return c.SendString(string(data))
+	})
+
+	app.Listen(":9999")
+}
